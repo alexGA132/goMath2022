ac60606ff1bae60dee1a89aa2c63aa5c1cd3b569
EmilleCorreia4 <evbc@discente.ifpe.edu.br>
2023-01-03 15:47:46 -0300

    estilização loginCadastro

---
+++ Simulado/eqPrimeiroGrau/questions.js
@@ -0,0 +1,181 @@
+export default [
+    {
+      question: "4x + 2 = 38",
+      //imageurl: "https://www.projetoagathaquestoes.com.br/imagens/provas-enem/matematica-pos/2017/o-resultado-de-uma-pesquisa-eleitoral-sobre-a-preferencia-a.webp",
+      answers: [
+        { option: " 9", correct: true },
+        { option: "2", correct: false },
+        { option: "4", correct: false },
+      ],
+    },
+    {
+      question: "9x = 6x + 12",
+      answers: [
+        { option: "3", correct: false },
+        { option: "4", correct: true },
+        { option: "12", correct: false },
+      ],
+    },
+    {
+      question: "5x – 1 = 3x + 11",
+      answers: [
+        { option: "6", correct: true },
+        { option: "6.5", correct: false },
+        { option: "5", correct: false },
+      ],
+    },
+    {
+      question: "2x + 8 = x + 13",
+      answers: [
+        { option: "0", correct: false },
+        { option: "8", correct: false },
+        { option: "5", correct: true },
+      ],
+    },
+    
+    {
+      question: "3 + x = 0",
+      answers: [
+        { option: "-3", correct: true },
+        { option: "8", correct: false },
+        { option: "0", correct: false },
+      ],
+    },
+    {
+      question: "23x + 2 = 2",
+      answers: [
+        { option: "0", correct: true },
+        { option: "8", correct: false },
+        { option: "5", correct: false },
+      ],
+    },
+    {
+      question: "12 – 7 + 4x = 25",
+      answers: [
+        { option: "0", correct: false },
+        { option: "8", correct: false },
+        { option: "5", correct: true },
+      ],
+    },
+    {
+      question: "5x – 3x = 30",
+      answers: [
+        { option: "45", correct: false },
+        { option: "15", correct: true },
+        { option: "5", correct: false },
+      ],
+    },
+    {
+      question: "4x + 10 = 45 – 3x ",
+      answers: [
+        { option: "32", correct: false },
+        { option: "6", correct: false },
+        { option: "5", correct: true },
+      ],
+    },
+    {
+      question: "2x + 4 = 2 – 3x",
+      answers: [
+        { option: "2/5", correct: false },
+        { option: "-2/5", correct: true },
+        { option: "2.5", correct: false },
+      ],
+    },
+    {
+      question: "5.(9 + y) = 20 – 3 + 6y",
+      answers: [
+        { option: "30", correct: false },
+        { option: "28", correct: true },
+        { option: "27.66", correct: false },
+      ],
+    },
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "4x - 1 = 3 (x -1",
+      answers: [
+        { option: "2", correct: false },
+        { option: "-1", correct: false },
+        { option: "-2", correct: true },
+      ],
+    },
+    
+    {
+      question: "4(x+10) - 2(x-5) = 0",
+      answers: [
+        { option: "12", correct: true },
+        { option: "-1", correct: false },
+        { option: "13", correct: false },
+      ],
+    },
+    
+    {
+      question: "(4x+6) - 2x = (x-6) + 10 + 14",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-6", correct: false },
+        { option: "6", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+    
+    {
+      question: "2x/(4 – 3x) = 2",
+      answers: [
+        { option: "0", correct: false },
+        { option: "-1", correct: false },
+        { option: "1", correct: true },
+      ],
+    },
+  ];
---
+++ Simulado/fracoes/questions.js
@@ -0,0 +1,35 @@
+export default [
+    {
+      question: "Quanto é 4/3 + 8/3?",
+      imageurl: "https://www.projetoagathaquestoes.com.br/imagens/provas-enem/matematica-pos/2017/o-resultado-de-uma-pesquisa-eleitoral-sobre-a-preferencia-a.webp",
+      answers: [
+        { option: "3", correct: false },
+        { option: "2", correct: false },
+        { option: "4", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 2/3 + 5/4?",
+      answers: [
+        { option: "7/9", correct: false },
+        { option: "23/12", correct: true },
+        { option: "23/7", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 8?",
+      answers: [
+        { option: "800", correct: false },
+        { option: "90", correct: false },
+        { option: "80", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+      ],
+    },
+  ];
